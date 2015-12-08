42f183666a52c664bbe31dd08868902f3abecf49
Jaime M <j7isaac@gmail.com>
2015-12-08 04:58:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cognitio::Application.config.secret_key_base = 'e3d272c7bc1c67c4b4cd3af146fea9f2d3f27f3afd75cf3a7b76f2066eeffb19ae8deaf98b51d10cdffb50c0c7b181eb6e4ce8c168bc3db52bf9ed4df5be8f72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cognitio::Application.config.session_store :cookie_store, key: '_cognitio_session'

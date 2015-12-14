bc5c54631e1c64606a36c3e7aa5339c68a59ab08
Jaime M <j7isaac@gmail.com>
2015-12-14 04:08:07 +0000

    Completed remaining permissions

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,26 +1,30 @@
 class Instructor::CoursesController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	def new
-		@course = Course.new
-	end
+  def new
+    @course = Course.new
+  end
 
-	def create
-    	@course = current_user.courses.create(course_params)
-    	if @course.valid?	
-    		redirect_to instructor_course_path(@course)
-    	else
-    		render :new, :status => :unprocessable_entity
-    	end
-  	end
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, :status => :unprocessable_entity
+    end
+  end
 
-  	def show
-  		@course = Course.find(params[:id])
-  	end
+  def show
+  end
 
-  	private
+  private
 
-  	def course_params
-  		params.require(:course).permit(:title, :description, :cost)
-  	end
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
 end

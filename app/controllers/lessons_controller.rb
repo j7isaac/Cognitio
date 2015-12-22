a52fa8cba5614bae7fc9cace22bf7b22bcb03300
Jaime M <j7isaac@gmail.com>
2015-12-22 03:23:55 +0000

    Setup a requirement for erollment on lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrollment_for_current_course, :only => [:show]
 	def show
 	end
 
@@ -7,4 +9,11 @@ class LessonsController < ApplicationController
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
+
+	def require_enrollment_for_current_course
+	    if current_user.enrolled_in?(current_lesson.section.course) != true
+	    	redirect_to course_path(current_lesson.section.course), :alert => 'Please enroll to begin lessons'
+	    end
+	end
+
 end
